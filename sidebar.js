a1d54d196ef2bd2e1f94302203aa2636f270ed1b
unknown <elekberzadeh333@gmail.com>
2021-03-10 10:34:06 +0400

    comlete

---
+++ sidebar.js
@@ -0,0 +1,15 @@
+document.querySelector('#closeCreateSidebar').addEventListener('click', () => {
+    document.querySelector('.sidebar--create-user').classList.remove('sidebar--active');
+})
+document.querySelector('#closeViewSidebar').addEventListener('click', () => {
+    document.querySelector('.sidebar--view-user').classList.remove('sidebar--active');
+})
+document.querySelector('#closeEditSidebar').addEventListener('click', () => {
+    document.querySelector('.sidebar--edit-user').classList.remove('sidebar--active');
+})
+document.querySelector('#swipeToEdit').addEventListener('click', () => {
+    document.querySelector('.sidebar--view-user').classList.remove('sidebar--active');
+    setTimeout(() => {
+        document.querySelector('.sidebar--edit-user').classList.add('sidebar--active');
+    }, 300);
+})
